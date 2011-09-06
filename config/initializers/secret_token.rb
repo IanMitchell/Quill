9888d36fa2294587da7b716f33256c805ec20719
Ian Mitchell <ian.mitchel1@live.com>
2011-09-06 16:46:58 -0700

    Initial Rails 3.1 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quill::Application.config.secret_token = '9912fa078b75108dde1f3192846df5c3779299ee76391613082ede6acd11f84aeaa51aee36008d5b143fefaa916afff258fa96180911bc8314854b6b94716ea3'
