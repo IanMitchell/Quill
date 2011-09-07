452feab75cd64e957d7e1df29ca9a5dc58dbf1bb
Ian Mitchell <ian.mitchel1@live.com>
2011-09-06 17:37:25 -0700

    Updated Database Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create(email: 'test@test.com', password: 'admin')
+Category.create(name: 'General')
+Post.create(title: 'Hello World!', )
+PostCategory.create(post_id: 1, category_id: 1, author_id: 1, content: "Hello World, from Quill!")
+Page.create(title: 'About Us', content: 'This is a test page', header: 1, sidebar: 1, rank: 1)
