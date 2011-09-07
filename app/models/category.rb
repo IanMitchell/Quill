8f60f01a9f7a6466fde40d8ae562d7b10e77dc47
Ian Mitchell <ian.mitchel1@live.com>
2011-09-06 17:05:00 -0700

    Added Categories

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ActiveRecord::Base
+  validates :name,      :presence => true,
+                        :uniqueness => true,
+                        :length => { :minimum => 3, :maximum => 75 }
+                        
+  has_many :post_categories
+  has_many :posts, :through => :post_categories
+  
+  extend FriendlyId
+  friendly_id :name, :use => :slugged
+end
