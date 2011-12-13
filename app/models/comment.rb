902f01d7f46fb65e79f86f8aaae45081d62b44dd
Ian Mitchell <ian.mitchel1@live.com>
2011-12-13 15:49:20 -0800

    Fixed URL being required for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,16 @@
 class Comment < ActiveRecord::Base
   include Gravtastic
-  
+
   validates :name,      :presence => true
-  
+
   validates :content,   :presence => true
-  
-  validates :website,   :format => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix }
-  
+
+  validates :website,   :format => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix },
+                        :allow_blank => true
+
   validates :email,     :presence => true,
                         :format => { :with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i }
-  
+
   belongs_to :post
   has_gravatar
 end
