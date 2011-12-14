6dbf1086346f41cd30fcbb6f2f5f353a8eb41513
Ian Mitchell <ian.mitchel1@live.com>
2011-12-13 16:06:21 -0800

    Fixed Markdown bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,8 +14,7 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, ↵
-      :no_intraemphasis, :fenced_code, :gh_blockcode]
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     syntax_highlighter(Redcarpet.new(text, ↵
       *options).to_html).html_safe
   end
