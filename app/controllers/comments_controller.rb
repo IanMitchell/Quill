9dada994c707d9fb4654ecd67d4d34fc4ba19b89
Ian Mitchell <ian.mitchel1@live.com>
2012-02-17 13:51:22 -0800

    Simplified Comment Deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,15 +10,12 @@ class CommentsController < ApplicationController
     respond_with @comment, :location => posts_path(@post)
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
-    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
     @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :ok }
-    end
+    respond_with @comment, :location => posts_path(@post)
   end
 end
