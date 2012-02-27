43b130279c01bb3ce4dec74e31f68271a1aaf9d7
Ian Mitchell <ian.mitchel1@live.com>
2012-02-27 14:07:57 -0800

    Added Admin Bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :admin_bar
+
+  private
+
+  def admin_bar
+  	@new_page = Page.new
+  	@new_post = Post.new
+  end
 end
