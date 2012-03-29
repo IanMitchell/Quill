7ceed114167c6248a23020beb754512da2f1816d
Ian Mitchell <ian.mitchel1@live.com>
2012-03-28 20:01:19 -0700

    Prevent Invisible Page Creation

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,12 +5,21 @@ class Page < ActiveRecord::Base
   
   validates :content,   :presence => true
   
-  validates :rank,     :uniqueness => true,
+  validates :rank,      :uniqueness => true,
                         :numericality => true
-                        
+
+  validate :is_visible
+
   extend FriendlyId
   friendly_id :title, :use => :slugged
 
   mount_uploader :icon, PageUploader
   
+
+  def is_visible
+    if self.header.blank? && self.sidebar.blank?
+      self.errors.add(:header, "Your page must be visible (choose Sidebar and/or Header).")
+      self.errors.add(:sidebar, "Your page must be visible (choose Sidebar and/or Header).")
+    end
+  end
 end
